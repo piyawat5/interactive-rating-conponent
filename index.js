a3d5213222987dc0a23deb66ab9b4701bd73a1bb
piyawat5 <98152492+piyawat5@users.noreply.github.com>
2022-03-24 14:32:19 +0700

    Add files via upload

--- index.js
+++ index.js
@@ -1,31 +1,41 @@
-function select(id) {
-  $(".number").removeClass("selected");
-  $(`#${id}`).addClass("selected");
-  $("button").click(function () {
-    $(".mainContainer").remove();
-
-    $("main").append($("<div>").addClass("mainContainer2"));
-    //new image
-    $("<img />", {
-      src: "images/illustration-thank-you.svg",
-      width: 120,
-    }).appendTo($(".mainContainer2"));
-    $(".mainContainer2").append(
-      $("<p>").addClass("pointContainer").text(`You selected ${id} out of 5`)
-    );
-
-    $(".mainContainer2").append($("<h1>").text(`Thank you!`));
-    $(".mainContainer2").append(
-      $("<p>").text(`We appreciate you taking the time to give a rating. If
-    you ever need more support, don’t hesitate to get in touch!`)
-    );
-  });
-}
+let selectPoint = 0;
 
 $("button").click(function () {
-  $(".pleaseSelect").text("* please rate 1-5");
-  $(".pleaseSelect").addClass("pleaseRate");
+  /// === 0
+  if (!selectPoint) {
+    return warning();
+  }
+  $(".mainContainer").remove();
+  $("main").append($("<div>").addClass("mainContainer").addClass("center"));
+  //new image
+  $("<img />", {
+    src: "images/illustration-thank-you.svg",
+    width: 120,
+  }).appendTo($(".mainContainer"));
+  $(".mainContainer").append(
+    $("<p>")
+      .addClass("pointContainer")
+      .text(`You selected ${selectPoint} out of 5`)
+  );
+
+  $(".mainContainer").append($("<h1>").text(`Thank you!`));
+  $(".mainContainer").append(
+    $("<p>").text(`We appreciate you taking the time to give a rating. If
+  you ever need more support, don’t hesitate to get in touch!`)
+  );
 });
+
+function warning() {
+  $(`.pleaseSelect`).text(`* please rate 1-5`).css(`color`, `red`);
+}
+
+function select(id) {
+  selectPoint = id;
+  console.log(id);
+
+  $(".number").removeClass("selected");
+  $(`#${id}`).addClass("selected");
+}
 
 //alternative
 // $(".number").click(function () {
